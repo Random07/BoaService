b9cc7dc5a6746b98b423b4f3db31ecba9c7dce39
Random07 <ourlifego@sina.com>
2018-03-13 21:31:28 +0800

    Apninfo.java

---
+++ src/com/mifi/boa/ApnInfo.java
@@ -0,0 +1,40 @@
+package com.mifi.boa;
+
+import android.util.Log;
+
+public class ApnInfo {
+    final String TAG = "BoaService_apn";
+    private int mIndex;
+    private String mName;
+    private String mApn;
+    private String mMcc;
+    private String mMnc;
+    private int mAuthTypeIndex;
+    private String mUserName;
+    private String mPassword;
+
+    public ApnInfo(int index,
+    					String name,
+    					String apn,
+    					String userName,
+    					String password,
+    					String mcc,
+    					String mnc,
+    					int authTypeIndex){
+    	Log.d(TAG, "apninfo index = " + index);
+    	mIndex = index;
+    	mName = name;
+    	mApn = apn;
+    	mMcc = mcc;
+        mMnc = mnc;
+        mAuthTypeIndex = authTypeIndex;
+        mUserName = userName;
+        mPassword = password;
+    }
+
+    public String toString(){
+    	return (mName + "|" + mApn + "|" +mMcc
+    			+ "|" +mMnc + "|" +mAuthTypeIndex
+    			+ "|" +mUserName + "|" +mPassword);
+    }
+}
