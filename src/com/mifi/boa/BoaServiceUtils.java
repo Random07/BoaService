b599e0b91946fb7933ea7261258d3535bfa45eef
xiaojun-he <279972007@qq.com>
2018-10-30 20:29:41 +0800

    更换获取mac地址借口

--- src/com/mifi/boa/BoaServiceUtils.java
+++ src/com/mifi/boa/BoaServiceUtils.java
@@ -2,9 +2,13 @@ package com.mifi.boa;
 
 import android.content.Context;
 import android.util.Log;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
-import android.net.wifi.WifiInfo;
+//import android.net.wifi.WifiConfiguration;
+//import android.net.wifi.WifiManager;
+//import android.net.wifi.WifiInfo;
+import java.util.List;
+import java.net.NetworkInterface;
+import java.util.Collections;
+
 
 public class BoaServiceUtils {
     final static String TAG = "BoaService_Utils";
@@ -13,26 +17,12 @@ public class BoaServiceUtils {
     static public String getLocalMacAddress(Context mContext)
     {
         String sRet="4G MIFI";
-        if(null != mContext){
-            Log.d(TAG, "mContext is null, so return null!");
-            return sRet;
-        }
-        WifiManager mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
-        if(null != mWifiManager){
-            Log.d(TAG, "mWifiManager is null, so return null!");
-            return sRet;
-        }
-        WifiInfo info = mWifiManager.getConnectionInfo();
-        if(null != info){
-            Log.d(TAG, "info is null, so return null!");
-            return sRet;
-        }
-        String mac =  info.getMacAddress();
+        String mac =  getNewMac();
         int strLen = 0;
 
         Log.d(TAG, "mac = " + mac);
 
-        if(null != mac){
+        if((null != mac)&&(!"".equals(mac))){
             mac = mac.replace(":","");
             Log.d(TAG, "remove separator,mac = " + mac);
 
@@ -47,5 +37,36 @@ public class BoaServiceUtils {
 
         Log.d(TAG, "return mac = " + sRet);
         return sRet;
-    }    
+    }
+
+    /**
+        * 通过网络接口取
+        * @return
+        */
+    private static String getNewMac() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return null;
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
